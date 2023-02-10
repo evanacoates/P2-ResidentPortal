101439255e3b02ee9d3fdd004aad4b5e96d62a7b
mollyturner <mollyturner50@gmail.com>
2023-02-10 01:47:52 -0600

    Add username and password attributes for residents model. Create password hashing hook before new resident creation.

--- models/residents.js
+++ models/residents.js
@@ -1,38 +1,56 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Residents extends Model {}
+class Residents extends Model { }
 
 Residents.init(
   {
     unitNumber: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       foreignKey: true,
       //autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
+      allowNull: true,
+    },
+    username: {
+      type: DataTypes.STRING,
       allowNull: false,
     },
     address: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
     },
     phoneNumber: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: (8),
+      },
+    },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+    },
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: 'residents',
-  }
-);
+  });
 
 module.exports = Residents;
