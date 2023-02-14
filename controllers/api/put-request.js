103d4af837e453742c01ebf99107f7be257a0b73
Evan <evanacoates@gmail.com>
2023-02-14 12:10:58 -0500

    Accounts put request

--- controllers/api/put-request.js
+++ controllers/api/put-request.js
@@ -32,3 +32,35 @@ router.put('/Leases', async (req, res) => {
               res.status(500).json(err);
             }
     
+
+            router.put('/Residents', async (req, res) => {
+                try {
+                    const dbUserData = await Residents.update(req.body, {
+                        where: {
+                          id: req.params.id,
+                        },
+                      });
+                      if (!dbuserData[0]) {
+                        res.status(404).json({ message: 'There are no residents with this user information.' });
+                        return;
+                      }
+                      res.status(200).json(dbUserData);
+                    } catch (err) {
+                      res.status(500).json(err);
+                    }
+            
+            router.put('/Accounts', async (req, res) => {
+                try {
+                    const dbUserData = await Accounts.update(req.body, {
+                    where: {
+                      id: req.params.id,
+                    },
+                    });
+                    if (!dbuserData[0]) {
+                    res.status(404).json({ message: 'There are no accounts with this user information' });
+                    return;
+                     }
+                    res.status(200).json(dbUserData);
+                    } catch (err) {
+                    res.status(500).json(err);
+                        }
