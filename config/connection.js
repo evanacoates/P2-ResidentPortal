67f6d627a04bd41fef252b95092650b22c3686ea
unknown <itsvictorg@gmail.com>
2023-02-15 21:02:31 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,5 +20,6 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
+}
 
 module.exports = sequelize;
