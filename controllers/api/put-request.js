3da2b156c35442160f4c1ed4b29414cbec35273c
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2023-02-16 12:05:22 -0600

    Put request attempt

--- controllers/api/put-request.js
+++ controllers/api/put-request.js
@@ -1,3 +1,4 @@
+const router = require('express').Router
 const { Accounts, Residents, Leases } = require("../../models");
 
 router.put('/leases', async (req, res) => {
@@ -24,10 +25,10 @@ router.put('/leases', async (req, res) => {
                   id: req.params.id,
                 },
               });
-              if (!dbuserData[0]) {
-                res.status(404).json({ message: 'There are no residents with this user information.' });
-                return;
-              }
+              // if (!dbuserData[0]) {
+              //   res.status(404).json({ message: 'There are no residents with this user information.' });
+              //   return;
+              // }
               res.status(200).json(dbUserData);
             } catch (err) {
               res.status(500).json(err);
@@ -35,22 +36,21 @@ router.put('/leases', async (req, res) => {
         })
     
 
-            router.put('/residents', async (req, res) => {
-                try {
-                    const dbUserData = await Residents.update(req.body, {
-                        where: {
-                          id: req.params.id,
-                        },
-                      });
-                      if (!dbuserData[0]) {
-                        res.status(404).json({ message: 'There are no residents with this user information.' });
-                        return;
-                      }
-                      res.status(200).json(dbUserData);
-                    } catch (err) {
-                      res.status(500).json(err);
-                    }
-                })
+            // router.put('/residents', async (req, res) => {
+            //     try {
+            //         const dbUserData = await Residents.update(req.body, {
+            //             where: {
+              //             },
+            //           });
+            //           if (!dbuserData[0]) {
+            //             res.status(404).json({ message: 'There are no residents with this user information.' });
+            //             return;
+            //           }
+            //           res.status(200).json(dbUserData);
+            //         } catch (err) {
+            //           res.status(500).json(err);
+            //         }
+            //     })
             
             router.put('/accounts', async (req, res) => {
                 try {
--- public/js/update.js
+++ public/js/update.js
@@ -1,42 +1,40 @@
 console.log('using update.js')
 
-async function lFHandler(event) {
+async function personHandler(event) {
     
     event.preventDefault();
-    
-    
     
     //const input = document.querySelector('.input-field').value.trim()
 
   
-     const input = document.querySelector('.input').value.trim()
-     console.log(input);
-    
-  
+     const name = document.getElementById('name').value.trim()
+     const email = document.getElementById('email').value.trim()
+     const phonenumber = document.getElementById('phone').value.trim()
+     const address = document.getElementById('address').value.trim()
 
-    console.log(input)
-    if(input){
+    if(name && email && phonenumber && address){
       
-      const response = await fetch('/api/put-request/', {
-        method: 'put',
-        body: JSON.stringify({ input }),
+      const response = await fetch('/api/put-requests/residents', {
+        method: 'PUT',
+        body: JSON.stringify({ name, email, phonenumber, address }),
         headers: { 'Content-Type': 'application/json' },
       });
-
-      console.log(input)
+      console.log(response);
   
-       if (response.ok) {
+    if (response.ok) {
         document.location.replace('/');
       } else {
         alert('Failed to update');
       }
     
-  } 
+  } else {
+    alert('Please fill out all fields (If same information re-enter)');
+  }
 }
 
   
   document
-    .getElementById('update')
-    .addEventListener('click', lFHandler);
+    .getElementById('update-person')
+    .addEventListener('click', personHandler);
   
 
