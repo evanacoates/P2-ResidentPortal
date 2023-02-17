979c25ef3de6e9609ed9518a919fa011577715b3
mollyturner <mollyturner50@gmail.com>
2023-02-16 23:20:44 -0600

    add log to see if envs are available

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// Add a log to see if ENVs are being added properly
+console.log("process.env.DB_NAME: ", process.env.DB_NAME)
+console.log("process.env.DB_USER: ", process.env.DB_USER)
+console.log("process.env.DB_PASSWORD: ", process.env.DB_PASSWORD)
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
